f33e61886b97ed8d858760219d14e3e1ab3f068c
shadowmoonezios <agushifelde@hotmail.com>
2025-11-06 14:13:42 +0200

    feat: initial commit for frontend/src/App.js

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Willkommen zur Reisen Buchungs App</h1>
+      <p>Planen Sie Ihre Reise mit uns!</p>
+    </div>
+  );
+}
+
+export default App;
